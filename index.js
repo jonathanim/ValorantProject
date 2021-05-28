b56b686a3e86ad154a187bd8a7327521f8822329
Jonathan <scrboy89@gmail.com>
2021-05-27 22:00:13 -0700

    first commit with images and maps

---
+++ index.js
@@ -0,0 +1,35 @@
+
+
+const agents = [
+    {
+    
+    name: "Astra",
+    img: "./images/Astra_artwork.png",
+    role: "Controller",
+    Gender: "Female"
+
+    },
+
+    {
+    
+        name: "Breach",
+        img: "./images/Breach_artwork.png",
+        role: "Initiator",
+        Gender: "Male"
+    
+        },
+    {
+    
+        name: "Breach",
+        img: "./images/Breach_artwork.png",
+        role: "Initiator",
+        Gender: "Male"
+    
+     },
+]
+
+
+
+
+
+
